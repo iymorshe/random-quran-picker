90fe7b4f23c5cf58552dc48d2a5f15875868f40b
Iman Morshed <iymorshed@ucdavis.edu>
2023-11-07 09:21:50 -0800

    Initial Commit

---
+++ random quran verse/random_quran_verseApp.swift
@@ -0,0 +1,20 @@
+//
+//  random_quran_verseApp.swift
+//  random quran verse
+//
+//  Created by Iman Morshed on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct random_quran_verseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
